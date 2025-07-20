df162b7b791cb4c8e99349f9ab12e480c505a273
DmytroBeridze <98057267+DmytroBeridze@users.noreply.github.com>
2025-07-20 23:09:12 +0300

    Button component added

--- client/src/shared/ui/atoms/Button/Button.tsx
+++ client/src/shared/ui/atoms/Button/Button.tsx
@@ -1,18 +1,24 @@
 interface IButtonProps {
+  type?: 'button' | 'submit' | 'reset';
   children: React.ReactNode;
   className?: string;
   variant?: 'primary' | 'secondary';
-  onClick?: () => void;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export const Button = ({
+  type = 'button',
   children,
   variant,
   onClick,
   className = '',
 }: IButtonProps) => {
   return (
-    <button onClick={onClick} className={`btn ${className} ${variant}`}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`btn ${className} ${variant}`}
+    >
       {/* <button onClick={onClick} className={className ?? ''}> */}
       {children}
     </button>
