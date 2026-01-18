8bca6f4d8d6b20c59ba304aa46936e76a6190f83
DmytroBeridze <98057267+DmytroBeridze@users.noreply.github.com>
2026-01-18 23:35:55 +0200

    feat(NFTPreview widget): make sales countdown responsive

--- client/src/entities/DiscountedWork/ui/SalesCountdown.tsx
+++ client/src/entities/DiscountedWork/ui/SalesCountdown.tsx
@@ -17,33 +17,49 @@ const SalesCountdown = ({ hours, minutes, seconds }: SalesCountdownType) => {
   return (
     <div
       className=" p-8 bg-secondary-background-color 
-            opacity-75 rounded-[20px]"
+            opacity-75 rounded-[20px]   "
     >
       <Text font="font-space-mono-regular" size="t-text-xs">
         Sale ends in:
       </Text>
 
-      <ul className="flex justify-between gap-3 ">
+      <ul className="grid grid-cols-[2fr_1fr_2fr_1fr_2fr] grid-rows-[2fr_1fr]  items-center justify-items-center">
+        {/* <ul className="flex  flex-wrap sm:flex-nowrap justify-between   gap-3 "> */}
         {countdownUnits.map((elem, i) => {
           return (
             <Fragment key={i}>
-              <li className="flex flex-col items-center">
-                <Text font="font-space-mono-bold" size="t-text-xl">
+              <li className="flex flex-col items-center row-start-1">
+                {/* ---------Numbers */}
+                <Text
+                  font="font-space-mono-bold"
+                  className="discountCountDown-number-responsive"
+                  // className="responsive-size-lg "
+                >
                   {elem.value}
                 </Text>
-                <Text font="font-space-mono-regular" size="t-text-xs">
+              </li>
+              {/* ---------Dots */}
+              {i < countdownUnits.length - 1 && (
+                <li className="row-start-1">
+                  <Text
+                    font="font-space-mono-bold"
+                    // size="t-text-lg"
+                    className="discountCountDown-number-responsive"
+                    // className="responsive-size-lg"
+                  >
+                    :
+                  </Text>
+                </li>
+              )}
+              {/* ---------Labels */}
+              <li
+                className="row-start-2"
+                style={{ gridColumn: `${i * 2 + 1}` }} // <-- label под цифрой
+              >
+                <Text font="font-space-mono-regular" size="responsive-size-xxs">
                   {elem.label}
                 </Text>
               </li>
-              {i < countdownUnits.length - 1 && (
-                <Text
-                  font="font-space-mono-bold"
-                  size="t-text-lg"
-                  className="mt-1.5"
-                >
-                  :
-                </Text>
-              )}
             </Fragment>
           );
         })}
--- client/src/widgets/NFTPreview/ui/NFTPreview.tsx
+++ client/src/widgets/NFTPreview/ui/NFTPreview.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react/jsx-runtime';
 import AuthorBage from '../../../entities/DiscountedWork/ui/AuthorBage';
 import SalesCountdown from '../../../entities/DiscountedWork/ui/SalesCountdown';
 import type { ISales } from '../../../entities/nft/model';
@@ -33,33 +34,57 @@ const NFTPreview = ({
         <div className=" mt-auto ">
           {/* -----------------author */}
           <AuthorBage avatar={avatar} userName={userName} className="mb-8" />
-          <div className="flex justify-between items-center ">
+          {/* <div className="flex justify-between  flex-col"> */}
+          <div className="nftPreview-content-responsive">
+            {/* <div className="flex flex-col gap-6 sm:flex-row sm:justify-between sm:items-center"> */}
+
             {/* ---------------content */}
-            <div className="flex flex-col gap-7">
+            <div className="flex flex-col gap-7 ">
               <Text
                 children={name}
                 font="font-work-sans-semibold"
                 size="t-text-2xl"
               />
+
+              {/* -------------smallSize -CountDown------- */}
+              <div className="discountCountDown-bigSize-hidden ">
+                <Countdown
+                  date={sales?.endAt ? new Date(sales?.endAt) : undefined}
+                  renderer={({ days, hours, minutes, seconds, completed }) => (
+                    <SalesCountdown
+                      // hours={24}
+                      hours={days * 24 + hours}
+                      minutes={minutes}
+                      seconds={seconds}
+                    />
+                  )}
+                />
+              </div>
+
+              {/* --------------------- */}
               <ButtonWithIcon
                 children="See NFT"
                 variant="secondary"
                 icon="eye-icon"
-                className="py-5 px-12 items-center cursor-pointer max-w-[198px] "
-                textClassName="text-inversive-text-color md:text-base text-xs "
+                className="py-5 px-12 items-center cursor-pointer nftPreview-button-responsive "
+                // className="py-5 px-12 items-center cursor-pointer max-w-[198px] "
+                textClassName="text-inversive-text-color text-base  "
+                radius="xl"
               />
             </div>
             {/* ---------------countdown */}
-            <Countdown
-              date={sales?.endAt ? new Date(sales?.endAt) : undefined}
-              renderer={({ days, hours, minutes, seconds, completed }) => (
-                <SalesCountdown
-                  hours={days * 24 + hours}
-                  minutes={minutes}
-                  seconds={seconds}
-                />
-              )}
-            />
+            <div className="discountCountDown-smallSize-hidden">
+              <Countdown
+                date={sales?.endAt ? new Date(sales?.endAt) : undefined}
+                renderer={({ days, hours, minutes, seconds, completed }) => (
+                  <SalesCountdown
+                    hours={days * 24 + hours}
+                    minutes={minutes}
+                    seconds={seconds}
+                  />
+                )}
+              />
+            </div>
           </div>
         </div>
       </InnerContainer>
