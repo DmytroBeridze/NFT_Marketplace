a1cfbb6b31047aef3ac8a9879b041b0f4b04dd50
DmytroBeridze <98057267+DmytroBeridze@users.noreply.github.com>
2026-05-20 18:46:55 +0300

    fix(ProtectedRoute):deleting a token on reboot

--- client/src/widgets/Header/ui/HeaderActions.tsx
+++ client/src/widgets/Header/ui/HeaderActions.tsx
@@ -4,11 +4,13 @@ import ThemeSwitcher from '../../../features/ThemeSwitcher/ui/ThemeSwitcher';
 
 import AuthButton from '../../../features/AuthButton/AuthButton';
 import { useNavigate } from 'react-router-dom';
-import { useAuthAction } from '../../../shared/lib/hooks';
+import { useAuthAction, useLocalStorage } from '../../../shared/lib/hooks';
 import { useAppDispatch, useAppSelector } from '../../../app/store/reduxHooks';
 import { clearUser } from '../../../entities/user/model';
 
 export const HeaderActions = () => {
+  const { removeLocal } = useLocalStorage();
+  const { loginLogoutHandler } = useAuthAction();
   const user = useAppSelector((state) => state.user.data);
   const dispatch = useAppDispatch();
   let navigate = useNavigate();
@@ -19,8 +21,10 @@ export const HeaderActions = () => {
       <LangSwitcher className="py-2 px-2" hover={false} />
 
       {/* <AuthButton handler={loginLogoutHandler} /> */}
+
       <AuthButton
         handler={() => {
+          removeLocal('token');
           user ? dispatch(clearUser()) : navigate('/auth');
         }}
       />
